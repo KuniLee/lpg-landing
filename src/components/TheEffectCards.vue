626df9e08866887430c5b433066f49c1389461bc
Sitnikov <selnetys@gmail.com>
2022-05-16 22:27:34 +0500

    added effect images

--- src/components/TheEffectCards.vue
+++ src/components/TheEffectCards.vue
@@ -2,29 +2,34 @@
   <div class="cards container-fluid">
     <h1 class="heading text-center">Эффект от процедуры</h1>
     <div class="row gy-3">
-      <div class="col-12">
-        <div class="card__effects"></div>
-      </div>
-      <div class="col-12">
-        <div class="card__effects"></div>
-      </div>
-      <div class="col-12">
-        <div class="card__effects"></div>
-      </div>
-      <div class="col-12">
-        <div class="card__effects"></div>
+      <div v-for="card in cards" :key="card.id" class="col-12">
+        <div class="card__effects position-relative">
+          <h2 class="text-center">{{ card.title }}</h2>
+          <img
+            class="w-100 position-absolute bottom-0"
+            :src="require(`@/images/efforts/${card.id}.png`)"
+            :alt="'effort_image_' + card.id"
+          />
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+import { useStore } from 'vuex'
+export default {
+  setup() {
+    const store = useStore()
+    const cards = store.state.effect.cards
+    return { cards }
+  },
+}
 </script>
 
 <style lang="scss">
 .card__effects {
-  height: 269px;
+  min-height: 269px;
   background-color: $white;
   border-radius: 20px;
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,10 @@
 import { createStore } from 'vuex'
+import effect from './modules/effect.module'
 
 export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  state: {},
+  getters: {},
+  mutations: {},
+  actions: {},
+  modules: { effect },
 })
